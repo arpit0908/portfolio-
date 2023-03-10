9a9b68a06c6303e76805c90337ccbfb29d5d8747
arpit0908 <arpitchauhan0908@gmail.com>
2023-03-10 19:54:32 +0530

    final commit for the portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white py-64"
+    >
+      <div className="max-w-screen-lg px-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl sm:mt-20 mt-10">
+        I am arpit chauhan ,I belong to new delhi .i am pursuing my graduation in computer science engineering from graphic era hill university,dehradun .
+        and i have completed my schooling from Babu bodhraj senoir secondary school noida  with an aggregate of 93 % in pcm  .
+        Talking about my strength i am well disciplaned and hardworking  person . My key accomplishment is that i participated in bharat ko jano  competition at state level & stood second in exam and won a price money of 
+        rupees 5100 and a certificate .apart from study i like listening music , playing badminton and travelling with my family .
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, ad iure velit vero natus voluptas quidem officiis ea incidunt. Impedit tempore soluta porro necessitatibus odio similique rem nobis ducimus aperiam quisquam explicabo, dignissimos voluptatum vel, aut aspernatur accusamus earum sunt iure provident asperiores veniam facilis. Quia quam dicta laudantium fuga!
+
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
