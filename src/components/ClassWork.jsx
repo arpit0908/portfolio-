2a7af4965ee634400febf986b4eaea1ced86211f
arpit0908 <arpitchauhan0908@gmail.com>
2023-06-10 01:39:23 +0530

    added ClassWork

--- src/components/ClassWork.jsx
+++ src/components/ClassWork.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Iframe from 'react-iframe'
 
 
 const Week = ()=>{
@@ -21,7 +22,79 @@ Second form of introduction is the one where we have to give our detailed inform
 
 At last we were asked to preapare our introductions as an assignment for next week</div>
       </div>
+      <div>
+        <h1 className='font-bold'> Week 3</h1>
+        <div>We had to prepare our introduction and speak it in front of everyone and at the end pf the class personal feedbacks were given to everyone.</div>
+      </div>
      </div>
+     <div> 
+        <h1 className='font-bold'>Week 4</h1>
+        <div>We were asked to write our introduction in 25 words with only the key points in it . <br />
+
+My introduction was: <br />
+
+Hi i'm Arpit Chauhan, Web developer with good command over C,C++,HTML,CSS and JavaScript .My projects are Sports portal, Hostel Mess Management System and Twitter Sentimental Analysis.</div>
+      </div>
+
+ <br />
+      <div> 
+        <h1 className='font-bold'>Week 5</h1>
+        <div>Discussion about how to tackle an interview, do's and dont's,dress code etc. were discussued.This discussion included insights of how to answer questions confidently and how to respond if we don't know the answer of any question. <br />
+
+Some of the major questions discussed were: <br />
+
+Tell me about yourself? <br />
+Where do you see yourself in next 5/10/15 years? <br />
+
+Who/What motivates you?</div>
+      </div>
+       <br />
+       <div> 
+        <h1 className='font-bold'>Week 6</h1>
+        <div>Discussion regarding techniques to ace interviews and how to answer questions was discussed. Answers to the questions given in our profiling sheet were also discussed.</div>
+      </div>
+ 
+<br />
+      
+      <div> 
+        <h1 className='font-bold'>Week 7</h1>
+        <div>The topic of group discussion was taught with all the tricks.<br></br>
+
+How we should do initiation <br />
+
+How to speak if we don't have points <br />
+
+How to present our views <br />
+
+How to make content based on the given topic</div>
+      </div>
+
+      <br />
+
+      <div> 
+        <h1 className='font-bold'>Week 8</h1>
+        <div>A group discussion was conducted based on given scenario.People were working in a coal mine suddenly a was started flooding into the mine,most of the people went out but 4 people were left behind Owner of the mine who is a man of around 45 and 1000 families depend on him for their employment,Second was a child around 10 who came their with his father who is a worker of the mine,Third was a female government officer with asthama problem and last one was a scientis who is in his last stage of cancer but has a discovery that can change the world's dependency on non renewable resources.
+
+Now the condition was that there is only a single lift and before the place fills up with water where these people are standing we can save only three out of the four people.</div>
+</div>
+ <br />
+
+ <div> 
+        <h1 className='font-bold'>Week 9</h1>
+        <div>Group discussions were conducted on topics like: <br />
+-Who is smarter men or women <br />
+
+-Should be banned beef in India  </div>
+</div>
+<br />
+
+<div> 
+        <h1 className='font-bold'>Week 10</h1>
+        <div> An activity was conducted in which our class was divided in groups of 7 people each and we were asked to act as the owners of a company with different roles for every team member.
+
+My team decided to act as the owner of  a company called Gaze.Ai which is a watch making company. I decided to act as the Quality testing manager of the company. We presented our company for around 7 minutes and links to the video are given in blog section</div>
+</div>
+<br />
 
   </div>
   )
@@ -44,7 +117,9 @@ const Blog = ({ title, description }) => {
 
 
     </div>
-  )
+  ) 
+
+  
 }
 
 
@@ -121,7 +196,7 @@ const ClassWork = () => {
               Blogs
             </p>
             <div>
-              <Blog title="Introduction" description="my nme is Arpit" />
+              <Blog title="" description="my name is Arpit" />
             </div>
           </div>
 
